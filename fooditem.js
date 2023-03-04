05570d6ea0644d8f19ee5f858cbde1d73eee24b8
Arryan <arryan2806@gmail.com>
2023-03-04 22:15:27 +0530

    Added cheatsheet

---
+++ fooditem.js
@@ -0,0 +1,251 @@
+const foodItem = [
+    {
+        id: 1,
+        name: 'Ambur Biryani',
+        category: 'biryani',
+        rating: 4.3,
+        price: 13,
+        img: '/biryani1.jpg',
+        quantity: 1
+    },
+    {
+        id: 2,
+        name: 'Hyderabadi Biryani',
+        category: 'biryani',
+        rating: 4.3,
+        price: 15,
+        img: '/biryani2.jpg',
+        quantity: 1
+    },
+    {
+        id: 3,
+        name: 'Classic Biryani',
+        category: 'biryani',
+        rating: 4.3,
+        price: 18,
+        img: '/biryani3.jpeg',
+        quantity: 1
+    },
+    {
+        id: 4,
+        name: 'Chatpata Biryani',
+        category: 'biryani',
+        rating: 4.3,
+        price: 15,
+        img: '/biryani4.jpeg',
+        quantity: 1
+    },
+    {
+        id: 5,
+        name: 'Tikha Biryani',
+        category: 'biryani',
+        rating: 4.3,
+        price: 10,
+        img: '/biryani5.jpeg',
+        quantity: 1
+    },
+    {
+        id: 6,
+        name: 'Kamrupi Biryani',
+        category: 'biryani',
+        rating: 4.3,
+        price: 12,
+        img: '/biryani6.jpeg',
+        quantity: 1
+    },
+    {
+        id: 7,
+        name: 'Kashmiri Biryani',
+        category: 'biryani',
+        rating: 4.3,
+        price: 13,
+        img: '/biryani7.png',
+        quantity: 1
+    },
+    {
+        id: 8,
+        name: 'Memoni Biryani',
+        category: 'biryani',
+        rating: 4.3,
+        price: 20,
+        img: '/biryani8.jpeg',
+        quantity: 1
+    },
+    {
+        id: 9,
+        name: 'Matar Paneer',
+        category: 'paneer',
+        rating: 4.3,
+        price: 15,
+        img: '/paneer1.jpeg',
+        quantity: 1
+
+    },
+    {
+        id: 10,
+        name: 'Palak Paneer',
+        category: 'paneer',
+        rating: 4.3,
+        price: 10,
+        img: '/paneer2.jpeg',
+        quantity: 1
+    },
+    {
+        id: 11,
+        name: 'Paneer Butter Masala',
+        category: 'paneer',
+        rating: 4.3,
+        price: 15,
+        img: '/paneer3.jpeg',
+        quantity: 1
+
+    },
+    {
+        id: 12,
+        name: 'Paneer Do Pyaza',
+        category: 'paneer',
+        rating: 4.3,
+        price: 12,
+        img: '/paneer4.jpeg',
+        quantity: 1
+
+    },
+    {
+        id: 13,
+        name: 'Hyderabadi Paneer',
+        category: 'paneer',
+        rating: 4.3,
+        price: 8,
+        img: '/paneer5.jpeg',
+        quantity: 1
+    },
+    {
+        id: 14,
+        name: 'Paneer Lababdar',
+        category: 'paneer',
+        rating: 4.3,
+        price: 7,
+        img: '/paneer6.jpeg',
+        quantity: 1
+    },
+    {
+        id: 15,
+        name: 'Shahi Paneer',
+        age: '32',
+        category: 'paneer',
+        rating: 4.3,
+        price: 5,
+        img: '/paneer7.jpeg',
+        quantity: 1
+    },
+    {
+        id: 16,
+        name: 'Momos',
+        category: 'chinese',
+        rating: 4.3,
+        price: 8,
+        img: '/momos.jpeg',
+        quantity: 1
+    },
+    {
+        id: 17,
+        name: 'Dry Manchurian',
+        category: 'chinese',
+        rating: 4.3,
+        price: 7,
+        img: '/manchurian.jpeg',
+        quantity: 1
+    },
+    {
+        id: 18,
+        name: 'Chili pot Rice',
+        category: 'chinese',
+        rating: 4.3,
+        price: 5,
+        img: '/rice1.jpeg',
+        quantity: 1
+    },
+    {
+        id: 19,
+        name: 'Chowmein',
+        category: 'chinese',
+        rating: 4.3,
+        price: 16,
+        img: '/chowmein.jpeg',
+        quantity: 1
+    },
+    {
+        id: 20,
+        name: 'Spring Roll',
+        category: 'chinese',
+        rating: 4.3,
+        price: 14,
+        img: '/sr.jpeg',
+        quantity: 1
+    },
+    {
+        id: 21,
+        name: 'Schezwan noodles',
+        category: 'chinese',
+        rating: 4.3,
+        price: 10,
+        img: '/noodles.jpeg',
+        quantity: 1
+    },
+    {
+        id: 22,
+        name: 'Fried Rice',
+        category: 'chinese',
+        rating: 4.3,
+        price: 8,
+        img: '/rice2.jpeg',
+        quantity: 1
+    },
+    {
+        id: 23,
+        name: 'Butter Masala Dosa',
+        category: 'south indian',
+        rating: 4.3,
+        price: 18,
+        img: '/dosa1.jpeg',
+        quantity: 1
+    },
+    {
+        id: 24,
+        name: 'Idli',
+        category: 'south indian',
+        rating: 4.3,
+        price: 20,
+        img: '/idli.jpeg',
+        quantity: 1
+    },
+    {
+        id: 25,
+        name: 'Onion Uttapam',
+        category: 'south indian',
+        rating: 4.3,
+        price: 15,
+        img: '/uttapa1.jpeg',
+        quantity: 1
+    },
+    {
+        id: 26,
+        name: 'Plain Dosa',
+        category: 'south indian',
+        rating: 4.3,
+        price: 40,
+        img: '/dosa2.jpeg',
+        quantity: 1
+    },
+    {
+        id: 27,
+        name: 'Rava Uttapam',
+        category: 'south indian',
+        rating: 4.3,
+        price: 25,
+        img: '/uttapa2.jpeg',
+        quantity: 1
+    },
+]
+
+export { foodItem };
